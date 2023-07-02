e69e49741a3c4edc6501948b24c3f9828df00549
dmackdev <dmackdev@gmail.com>
2023-07-02 08:20:38 +0100

    added client request editor

--- src/game_ui.rs
+++ src/game_ui.rs
@@ -2,11 +2,16 @@ use bevy::prelude::{
     in_state, App, EventWriter, IntoSystemConfig, IntoSystemSetConfig, Plugin, SystemSet,
 };
 use bevy_egui::{egui, EguiContexts};
+use strum::IntoEnumIterator;
 
 use crate::{
     events::{AddComponentEvent, StartSimulationEvent},
     game_state::GameState,
-    node::{client::Client, server::Server, NodeName, NodeType},
+    node::{
+        client::{Client, HttpMethod, RequestConfig},
+        server::Server,
+        NodeName, NodeType,
+    },
 };
 
 use bevy::{input::common_conditions::input_toggle_active, prelude::*};
@@ -113,14 +118,80 @@ impl View for NodeType {
 
 impl View for Client {
     fn ui(&mut self, ui: &mut egui::Ui) {
-        ui.add(
-            egui::TextEdit::multiline(&mut self.config)
-                .font(egui::TextStyle::Monospace) // for cursor height
-                .code_editor()
-                .desired_rows(10)
-                .lock_focus(true)
-                .desired_width(f32::INFINITY),
-        );
+        ui.separator();
+
+        ui.heading("Requests");
+        ui.separator();
+
+        let format_method = |method: HttpMethod| method.to_string().to_ascii_uppercase();
+
+        for (idx, config) in self.request_configs.iter_mut().enumerate() {
+            ui.horizontal(|ui| {
+                ui.label("URL:");
+                ui.text_edit_singleline(&mut config.url);
+            });
+
+            ui.horizontal(|ui| {
+                ui.label("Path:");
+                ui.text_edit_singleline(&mut config.path);
+            });
+
+            ui.horizontal(|ui| {
+                ui.label("Method:");
+
+                egui::ComboBox::from_id_source(idx)
+                    .selected_text(format_method(config.method))
+                    .show_ui(ui, |ui| {
+                        for method in HttpMethod::iter() {
+                            ui.selectable_value(&mut config.method, method, format_method(method));
+                        }
+                    });
+            });
+
+            match config.method {
+                HttpMethod::Get => {
+                    ui.label("Params:");
+
+                    let mut params_idx_to_delete = vec![];
+
+                    for (idx, (id, val)) in config.params.iter_mut().enumerate() {
+                        ui.columns(2, |cols| {
+                            cols[0].horizontal(|ui| {
+                                if ui.button("x").clicked() {
+                                    params_idx_to_delete.push(idx);
+                                }
+                                ui.text_edit_singleline(id);
+                            });
+                            cols[1].text_edit_singleline(val);
+                        });
+                    }
+
+                    for i in params_idx_to_delete.into_iter() {
+                        config.params.remove(i);
+                    }
+
+                    if ui.button("Add Param").clicked() {
+                        config.params.push(("".to_string(), "".to_string()));
+                    }
+                }
+                HttpMethod::Post => {
+                    ui.label("Body:");
+                    ui.add(
+                        egui::TextEdit::multiline(&mut config.body)
+                            .font(egui::TextStyle::Monospace) // for cursor height
+                            .code_editor()
+                            .desired_rows(10)
+                            .lock_focus(true)
+                            .desired_width(f32::INFINITY),
+                    );
+                }
+            }
+
+            ui.separator();
+        }
+        if ui.button("Add Request").clicked() {
+            self.request_configs.push(RequestConfig::default());
+        }
     }
 }
 
--- src/node/client.rs
+++ src/node/client.rs
@@ -1,10 +1,29 @@
+use bevy::utils::HashMap;
+use strum::{Display, EnumIter};
+
 #[derive(Clone, Debug, Default)]
 pub struct Client {
-    pub config: String,
+    pub request_configs: Vec<RequestConfig>,
 }
 
 impl Client {
     pub fn new() -> Self {
         Default::default()
     }
+}
+
+#[derive(Clone, Debug, Default)]
+pub struct RequestConfig {
+    pub url: String,
+    pub path: String,
+    pub method: HttpMethod,
+    pub body: String,
+    pub params: Vec<(String, String)>,
+}
+
+#[derive(Clone, Copy, Debug, Default, PartialEq, Eq, EnumIter, Display)]
+pub enum HttpMethod {
+    #[default]
+    Get,
+    Post,
 }
